5feaa7c63d0189c4a14842a0e083a6449b8cdf9a
NordCreativeGL <oskarvedel5@gmail.com>
2026-05-21 21:40:41 -0100

    wordmark 130vw

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -40,7 +40,7 @@ export default function HeroSection() {
       tl.to(wm, {
         opacity: 1,
         filter: "blur(0px)",
-        width: "100vw",
+        width: "130vw",
         duration: 1.2,
         ease: "power2.out",
       }, 2.0);
@@ -141,8 +141,8 @@ export default function HeroSection() {
             draggable={false}
             style={{
               display: "block",
-              width: "100vw",
-              maxWidth: "100vw",
+              width: "130vw",
+              maxWidth: "130vw",
               height: "auto",
               opacity: 0,
               filter: "blur(12px)",
