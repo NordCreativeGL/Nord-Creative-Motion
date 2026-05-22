f97d48d6b6e09a33c68adfefceac7d1770bcebf3
NordCreativeGL <oskarvedel5@gmail.com>
2026-05-22 22:38:55 -0100

    auto: claude changes

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -45,7 +45,7 @@ export default function HeroSection() {
 
     tl.to(ringDiv, {
       x: 440.54 - centerX,
-      y: 466.27 - centerY,
+      y: 458.27 - centerY,
       scale: ringScale,
       duration: 1.5,
       ease: 'power2.inOut',
@@ -54,7 +54,7 @@ export default function HeroSection() {
 
     tl.to(needleDiv, {
       x: 1218.67 - centerX,
-      y: 467.14 - centerY,
+      y: 459.14 - centerY,
       scale: needleScale,
       duration: 1.5,
       ease: 'power2.inOut',
