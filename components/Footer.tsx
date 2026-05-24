8a5977b8c22bc4cd6eb4ba05612a20efff8953d8
NordCreativeGL <oskarvedel5@gmail.com>
2026-05-24 19:27:34 -0100

    auto: claude changes

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -9,9 +9,9 @@ export default function Footer() {
         <Image
           src="/logo-wordmark-transparent.png"
           alt="NordCreative"
-          width={300}
-          height={90}
-          className="w-[300px] h-auto"
+          width={510}
+          height={153}
+          className="w-[510px] h-auto"
         />
 
         <p className="text-lg text-white/40">
