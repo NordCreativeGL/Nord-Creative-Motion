338dd78e534398ed3eae563ffd8bec38cf4ce07f
NordCreativeGL <oskarvedel5@gmail.com>
2026-05-30 15:53:08 -0100

    auto: claude changes

--- components/MountainSilhouette.tsx
+++ components/MountainSilhouette.tsx
@@ -52,64 +52,47 @@ export default function MountainSilhouette() {
       }}
     >
       <defs>
-        <linearGradient id="backGrad" gradientUnits="userSpaceOnUse" x1="0" y1="220" x2="0" y2="500">
-          <stop offset="0%" stopColor="#121220" />
+        <linearGradient id="gradL1" gradientUnits="userSpaceOnUse" x1="0" y1="272" x2="0" y2="500">
+          <stop offset="0%" stopColor="#1c1c2a" />
           <stop offset="100%" stopColor="#000000" />
         </linearGradient>
-        <linearGradient id="midGrad" gradientUnits="userSpaceOnUse" x1="0" y1="160" x2="0" y2="500">
-          <stop offset="0%" stopColor="#0e0e1a" />
+        <linearGradient id="gradL2" gradientUnits="userSpaceOnUse" x1="0" y1="225" x2="0" y2="500">
+          <stop offset="0%" stopColor="#141420" />
           <stop offset="100%" stopColor="#000000" />
         </linearGradient>
-        <linearGradient id="frontGrad" gradientUnits="userSpaceOnUse" x1="0" y1="0" x2="0" y2="500">
-          <stop offset="0%" stopColor="#0c0c18" />
-          <stop offset="20%" stopColor="#030306" />
+        <linearGradient id="gradL3" gradientUnits="userSpaceOnUse" x1="0" y1="145" x2="0" y2="500">
+          <stop offset="0%" stopColor="#0d0d18" />
           <stop offset="100%" stopColor="#000000" />
         </linearGradient>
-        <filter id="mtnDepth" x="-5%" y="-20%" width="110%" height="130%">
-          <feDropShadow dx="0" dy="-8" stdDeviation="14" floodColor="#000000" floodOpacity="0.5" />
-        </filter>
+        <linearGradient id="gradL4" gradientUnits="userSpaceOnUse" x1="0" y1="88" x2="0" y2="500">
+          <stop offset="0%" stopColor="#080810" />
+          <stop offset="100%" stopColor="#000000" />
+        </linearGradient>
+        <linearGradient id="gradL5" gradientUnits="userSpaceOnUse" x1="0" y1="68" x2="0" y2="500">
+          <stop offset="0%" stopColor="#040408" />
+          <stop offset="100%" stopColor="#000000" />
+        </linearGradient>
       </defs>
 
-      {/* Back: smooth rolling hills, peaks y=230–275 */}
       <path
-        d="M 0,500 L 0,292
-           C 120,292 200,254 280,252
-           C 360,250 432,285 560,248
-           C 668,220 762,268 904,264
-           C 1046,260 1120,246 1302,253
-           C 1482,260 1584,283 1728,286
-           L 1728,500 Z"
-        fill="url(#backGrad)"
+        d="M 0,500 L 0,375 L 105,272 L 132,272 L 275,375 L 432,265 L 460,265 L 612,375 L 755,268 L 782,268 L 930,375 L 1068,272 L 1095,272 L 1235,375 L 1382,268 L 1408,268 L 1728,500 Z"
+        fill="url(#gradL1)"
       />
-
-      {/* Middle: left wide rounded (x≈380, y≈178), right angular (x≈1050, y≈160) */}
       <path
-        d="M 0,500 L 0,362
-           C 98,360 218,202 380,178
-           C 542,154 618,342 722,340
-           C 826,338 900,182 1050,160
-           C 1200,138 1304,336 1454,332
-           C 1554,328 1632,343 1728,350
-           L 1728,500 Z"
-        fill="url(#midGrad)"
+        d="M 0,500 L 0,352 L 78,278 L 102,278 L 195,358 L 298,225 L 328,225 L 442,358 L 568,248 L 598,248 L 718,358 L 858,228 L 892,228 L 1012,355 L 1148,242 L 1182,242 L 1318,358 L 1448,235 L 1482,235 L 1728,500 Z"
+        fill="url(#gradL2)"
       />
-
-      {/* Front: spire (x≈260,y=122), dome (x≈640,y=192), dominant (x≈944,y=50), low hill (x≈1250,y=252), ridge (x≈1560,y=148) */}
       <path
-        d="M 0,500 L 0,392
-           C 78,390 218,128 260,122
-           C 302,116 386,344 432,344
-           C 484,344 572,193 640,192
-           C 706,191 764,316 820,316
-           C 864,316 906,60 944,50
-           C 982,40 1024,326 1094,330
-           C 1164,334 1206,254 1250,252
-           C 1296,250 1367,319 1404,319
-           C 1436,319 1492,150 1562,148
-           C 1628,146 1700,353 1728,370
-           L 1728,500 Z"
-        fill="url(#frontGrad)"
-        filter="url(#mtnDepth)"
+        d="M 0,500 L 0,265 L 88,178 L 115,178 L 242,278 L 372,145 L 408,145 L 555,270 L 685,172 L 718,172 L 868,282 L 1032,152 L 1068,152 L 1222,272 L 1362,162 L 1395,162 L 1728,500 Z"
+        fill="url(#gradL3)"
+      />
+      <path
+        d="M 0,500 L 0,282 L 105,165 L 135,165 L 278,295 L 405,88 L 442,88 L 588,282 L 728,155 L 760,155 L 912,298 L 1060,95 L 1098,95 L 1255,282 L 1398,168 L 1432,168 L 1728,500 Z"
+        fill="url(#gradL4)"
+      />
+      <path
+        d="M 0,500 L 0,300 L 72,198 L 98,198 L 148,255 L 235,138 L 262,138 L 345,268 L 418,172 L 445,172 L 520,292 L 608,68 L 638,68 L 750,272 L 828,162 L 858,162 L 952,298 L 1048,122 L 1082,122 L 1198,282 L 1305,195 L 1338,195 L 1452,338 L 1562,218 L 1598,218 L 1728,338 L 1728,500 Z"
+        fill="url(#gradL5)"
       />
     </svg>
   );
